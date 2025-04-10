eaa5ac6cfd3dda42d0cb968677d9a1baa62a2b38
Ionut Ojica <contact@ionutojica.ro>
2025-04-10 17:25:27 +0200

    new clean script

---
+++ scripts/script-e40-clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###############################################################################################
+#####                        Clear temporary and not needed files                         #####
+###############################################################################################
+
+
+show_info ${ICON_INFO} 'Autoremove of not needed packages...'
+DEBIAN_FRONTEND=noninteractive apt-get -yq autoremove >/dev/null
+show_info ${ICON_INFO} 'Clear cache of installed packages...'
+DEBIAN_FRONTEND=noninteractive apt-get -yq clean >/dev/null
+show_info ${ICON_OK} 'Autoremove and clean finished.'
