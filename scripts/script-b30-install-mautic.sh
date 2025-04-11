b9f48ea9ef8ee90670e7e973ef4816f4e22d3cc0
Ionut Ojica <contact@ionutojica.ro>
2025-04-11 22:10:12 +0200

    install also amazon-mailer

--- scripts/script-b30-install-mautic.sh
+++ scripts/script-b30-install-mautic.sh
@@ -4,17 +4,16 @@
 #####                                 Install Mautic                                      #####
 ###############################################################################################
 
-show_info ${ICON_INFO} 'Download Mautic...'
+show_info ${ICON_INFO} 'Download and install Mautic - this will take time...'
 
-mkdir -p "${MAUTIC_FOLDER}"
+COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "${MAUTIC_FOLDER}" --no-interaction
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "$MAUTIC_FOLDER" --no-interaction
+chown -R www-data:www-data "${MAUTIC_FOLDER}"
+chmod -R 755 "${MAUTIC_FOLDER}"
 
-# DEBIAN_FRONTEND=noninteractive apt-get -yq install unzip zip >/dev/null
-# wget -q "${MAUTIC_DOWNLOAD_URL}" -P "${INSTALL_FOLDER}"
-#
-# show_info ${ICON_INFO} 'Unzip Mautic...'
-# unzip -q "${INSTALL_FOLDER}${MAUTIC_VERSION}.zip" -d "${MAUTIC_FOLDER}"
+show_info ${ICON_INFO} 'Download and install symfony/amazon-mailer...'
+
+COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer --working-dir="${MAUTIC_FOLDER}" require symfony/amazon-mailer --no-interaction
 
 chown -R www-data:www-data "${MAUTIC_FOLDER}"
 chmod -R 755 "${MAUTIC_FOLDER}"
