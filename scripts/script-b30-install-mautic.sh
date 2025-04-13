4fc3151c1a39a12e120bdc74030eed11aca5da9a
Ionut Ojica <contact@ionutojica.ro>
2025-04-13 21:19:16 +0200

    feat: refactor installation script to handle Mautic extensions dynamically

--- scripts/script-b30-install-mautic.sh
+++ scripts/script-b30-install-mautic.sh
@@ -10,21 +10,33 @@ COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer create-projec
 
 if [[ $? -ne 0 ]]; then
   show_info ${ICON_ERR} "Error: Installation of Mautic${MAUTIC_COUNT} failed."
-  show_info ${ICON_ERR} "Should the installation continue?"
+  show_info ${ICON_QUE} "Should the installation continue?"
   answer_yes_else_stop
 fi
 
 chown -R www-data:www-data "${MAUTIC_FOLDER}"
 chmod -R 755 "${MAUTIC_FOLDER}"
 
-show_info ${ICON_INFO} 'Download and install symfony/amazon-mailer...'
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer --working-dir="${MAUTIC_FOLDER}" require symfony/amazon-mailer --no-interaction > /dev/null 2>&1
+show_info ${ICON_INFO} "Installing Mautic extensions listed in config..."
 
-if [[ $? -ne 0 ]]; then
-  show_info ${ICON_ERR} "Error: Installation of symfony/amazon-mailer for Mautic${MAUTIC_COUNT} failed."
-  show_info ${ICON_ERR} "Should the installation continue?"
-  answer_yes_else_stop
+if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
+  for extension in "${MAUTIC_EXTENSIONS[@]}"; do
+    show_info ${ICON_INFO} "Installing extension: ${extension}..."
+
+    COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
+    composer --working-dir="${MAUTIC_FOLDER}" require "$extension" --no-interaction > /dev/null 2>&1
+
+    if [[ $? -ne 0 ]]; then
+      show_info ${ICON_ERR} "Error: Installation of ${extension} for Mautic${MAUTIC_COUNT} failed."
+      show_info ${ICON_QUE} "Should the installation continue?"
+      answer_yes_else_stop
+    else
+      show_info ${ICON_OK} "Extension ${extension} installed successfully."
+    fi
+  done
+else
+  show_info ${ICON_WARN} "No Mautic extensions defined in config."
 fi
 
 chown -R www-data:www-data "${MAUTIC_FOLDER}"
