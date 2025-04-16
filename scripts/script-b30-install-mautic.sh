6f3df05f55557714ba7b54c3b5cce04219460666
Ionut Ojica <contact@ionutojica.ro>
2025-04-17 00:28:01 +0200

    feat: refactor installation script for improved clarity and performance

--- scripts/script-b30-install-mautic.sh
+++ scripts/script-b30-install-mautic.sh
@@ -6,7 +6,16 @@
 
 show_info ${ICON_INFO} 'Download and install Mautic - this will take time...'
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "${MAUTIC_FOLDER}" --no-interaction > /dev/null 2>&1
+mkdir -p "${MAUTIC_FOLDER}"
+chown -R www-data:www-data "${MAUTIC_FOLDER}"
+
+
+export COMPOSER_ALLOW_SUPERUSER=1
+export COMPOSER_PROCESS_TIMEOUT=10000
+
+runuser -u www-data -- \
+composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "${MAUTIC_FOLDER}" \
+  --no-interaction > /dev/null 2>&1
 
 if [[ $? -ne 0 ]]; then
   show_info ${ICON_ERR} "Error: Installation of Mautic${MAUTIC_COUNT} failed."
@@ -14,32 +23,33 @@ if [[ $? -ne 0 ]]; then
   answer_yes_else_stop
 fi
 
-chown -R www-data:www-data "${MAUTIC_FOLDER}"
-chmod -R 755 "${MAUTIC_FOLDER}"
+#chown -R www-data:www-data "${MAUTIC_FOLDER}"
+#chmod -R 755 "${MAUTIC_FOLDER}"
 
 
 show_info ${ICON_INFO} "Installing Mautic extensions listed in config..."
 
 if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
-  for extension in "${MAUTIC_EXTENSIONS[@]}"; do
-    show_info ${ICON_INFO} "Installing extension: ${extension}..."
+  show_info ${ICON_INFO} "Preparing to install extensions: ${MAUTIC_EXTENSIONS[*]}"
 
-    COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
-    composer --working-dir="${MAUTIC_FOLDER}" require "$extension" --no-interaction > /dev/null 2>&1
+  # Install all extensions in a single Composer command
+  runuser -u www-data -- \
+  composer --working-dir="${MAUTIC_FOLDER}" require "${MAUTIC_EXTENSIONS[@]}" \
+    --no-interaction --optimize-autoloader --no-scripts > /dev/null 2>&1
 
-    if [[ $? -ne 0 ]]; then
-      show_info ${ICON_ERR} "Error: Installation of ${extension} for Mautic${MAUTIC_COUNT} failed."
-      show_info ${ICON_QUE} "Should the installation continue?"
-      answer_yes_else_stop
-    else
-      show_info ${ICON_OK} "Extension ${extension} installed successfully."
-    fi
-  done
+  if [[ $? -ne 0 ]]; then
+    show_info ${ICON_ERR} "Error: One or more extensions failed to install."
+    show_info ${ICON_QUE} "Should the installation continue?"
+    answer_yes_else_stop
+  else
+    show_info ${ICON_OK} "All extensions installed successfully."
+  fi
+
 else
   show_info ${ICON_WARN} "No Mautic extensions defined in config."
 fi
 
-chown -R www-data:www-data "${MAUTIC_FOLDER}"
-chmod -R 755 "${MAUTIC_FOLDER}"
+#chown -R www-data:www-data "${MAUTIC_FOLDER}"
+#chmod -R 755 "${MAUTIC_FOLDER}"
 
 show_info ${ICON_OK} 'Mautic is installed.'
