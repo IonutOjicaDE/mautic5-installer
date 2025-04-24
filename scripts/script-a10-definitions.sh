c7f210bb07e21c4ebf05fbf8203b6fd8587c9ca2
Ionut Ojica <contact@ionutojica.ro>
2025-04-24 18:28:26 +0200

    refactor: remove redundant installation prompts from script

--- scripts/script-a10-definitions.sh
+++ scripts/script-a10-definitions.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-VERSION="0.0.2"
+VERSION="0.0.3"
 show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 ###############################################################################################
 #####                                      EMPTY                                          #####
 ###############################################################################################
-
-show_info ${ICON_ERR} "Test of the retry installation."
-show_info ${ICON_QUE} "Stop the installation now?"
-answer_yes_else_stop
