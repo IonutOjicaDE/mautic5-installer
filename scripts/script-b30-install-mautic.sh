bd9d173f05b4cb70fecef94e9eb7926c9e2a1fde
Ionut Ojica <contact@ionutojica.ro>
2025-04-26 19:58:59 +0200

    refactor: improve clarity of installation messages in Mautic script

--- scripts/script-b30-install-mautic.sh
+++ scripts/script-b30-install-mautic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-VERSION="0.0.2"
+VERSION="0.0.3"
 show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 ###############################################################################################
 #####                                 Install Mautic                                      #####
 ###############################################################################################
 
-show_info ${ICON_INFO} 'Download and install Mautic - this will take time...'
+show_info ${ICON_INFO} 'Download and install Mautic - this will take time (~2 min)...'
 
 COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
 composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "${MAUTIC_FOLDER}" \
@@ -21,12 +21,11 @@ fi
 chown -R www-data:www-data "${MAUTIC_FOLDER}"
 chmod -R 755 "${MAUTIC_FOLDER}"
 
-show_info ${ICON_OK} ' done.' 0
+show_info ${ICON_OK} 'done.' 0
 
-show_info ${ICON_INFO} "Installing Mautic extensions listed in config..."
 
 if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
-  show_info ${ICON_INFO} "Preparing to install extensions: ${MAUTIC_EXTENSIONS[*]}"
+  show_info ${ICON_INFO} "Installing extensions: ${MAUTIC_EXTENSIONS[*]}..."
 
   # Install all extensions in a single Composer command
   COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
@@ -34,16 +33,31 @@ if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
     --no-interaction --optimize-autoloader --no-scripts > /dev/null 2>&1
 
   if [[ $? -ne 0 ]]; then
-    show_info ${ICON_ERR} "Error: One or more extensions failed to install."
-    show_info ${ICON_QUE} "Should the installation continue?"
+    show_info ${ICON_ERR} " failed." 0
+    show_info ${ICON_QUE} "Should we try installing one after the other to find out which one failed?"
     answer_yes_else_stop
+
+    for extension in "${MAUTIC_EXTENSIONS[@]}"; do
+      show_info ${ICON_INFO} "Installing extension: ${extension}..."
+      COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
+      composer --working-dir="${MAUTIC_FOLDER}" require "${extension}" \
+        --no-interaction --optimize-autoloader --no-scripts > /dev/null 2>&1
+
+      if [[ $? -ne 0 ]]; then
+        show_info ${ICON_ERR} " failed." 0
+        show_info ${ICON_QUE} "Should we continue with the next extension?"
+        answer_yes_else_stop
+      else
+        show_info ${ICON_OK} 'done.' 0
+      fi
+    done
   else
-    show_info ${ICON_OK} "All extensions installed successfully."
+    show_info ${ICON_OK} 'done.' 0
   fi
 
   chown -R www-data:www-data "${MAUTIC_FOLDER}"
   chmod -R 755 "${MAUTIC_FOLDER}"
 
 else
-  show_info ${ICON_WARN} "No Mautic extensions defined in config."
+  show_info ${ICON_OK} "No Mautic extensions defined in config file to install."
 fi
