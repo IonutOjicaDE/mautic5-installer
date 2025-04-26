5ff61d370b380489c9e15138d90ade48b2596693
Ionut Ojica <contact@ionutojica.ro>
2025-04-26 20:40:40 +0200

    refactor: improve error reporting and output handling during Mautic installation

--- scripts/script-b30-install-mautic.sh
+++ scripts/script-b30-install-mautic.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-VERSION="0.0.3"
-show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
+VERSION="0.0.4"
+show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}" 1
 
 ###############################################################################################
 #####                                 Install Mautic                                      #####
 ###############################################################################################
 
-show_info ${ICON_INFO} 'Download and install Mautic - this will take time (~2 min)...'
+show_info ${ICON_INFO} 'Installing Mautic - this will take time (~2 min)...'
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
+output=$(COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
 composer create-project mautic/recommended-project:"${MAUTIC_VERSION}" "${MAUTIC_FOLDER}" \
-  --no-interaction > /dev/null 2>&1
+  --no-interaction 2>&1)
 
 if [[ $? -ne 0 ]]; then
+  show_info ${ICON_ERR} 'failed.' 0
   show_info ${ICON_ERR} "Error: Installation of Mautic${MAUTIC_COUNT} failed."
-  show_info ${ICON_QUE} "Should the installation continue?"
+  echo -e "\n--- Composer output ---"
+  echo "$output"
+  echo '------------------------'
+  show_info ${ICON_QUE} 'Should the installation continue?'
   answer_yes_else_stop
 fi
 
@@ -28,13 +32,13 @@ if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
   show_info ${ICON_INFO} "Installing extensions: ${MAUTIC_EXTENSIONS[*]}..."
 
   # Install all extensions in a single Composer command
-  COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
+  output=$(COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 \
   composer --working-dir="${MAUTIC_FOLDER}" require "${MAUTIC_EXTENSIONS[@]}" \
-    --no-interaction --optimize-autoloader --no-scripts > /dev/null 2>&1
+    --no-interaction --optimize-autoloader --no-scripts 2>&1)
 
   if [[ $? -ne 0 ]]; then
-    show_info ${ICON_ERR} " failed." 0
-    show_info ${ICON_QUE} "Should we try installing one after the other to find out which one failed?"
+    show_info ${ICON_ERR} 'failed.' 0
+    show_info ${ICON_QUE} 'Should we try installing one after the other to find out which one failed?'
     answer_yes_else_stop
 
     for extension in "${MAUTIC_EXTENSIONS[@]}"; do
@@ -46,6 +50,9 @@ if [[ -n "${MAUTIC_EXTENSIONS[*]}" ]]; then
       if [[ $? -ne 0 ]]; then
         show_info ${ICON_ERR} " failed." 0
         show_info ${ICON_QUE} "Should we continue with the next extension?"
+        echo -e "\n--- Composer output ---"
+        echo "$output"
+        echo "------------------------"
         answer_yes_else_stop
       else
         show_info ${ICON_OK} 'done.' 0
