59d78bda14152822c028068e02744bd28fbe267c
Ionut Ojica <contact@ionutojica.ro>
2025-04-26 20:00:21 +0200

    refactor: remove extra spaces in output messages and suppress command output in cleanup script

--- scripts/script-e40-clean.sh
+++ scripts/script-e40-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="0.0.3"
+VERSION="0.0.4"
 show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 ###############################################################################################
@@ -12,22 +12,22 @@ COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_PROCESS_TIMEOUT=10000 composer --working-dir
 if [[ $? -ne 0 ]]; then
   show_info ${ICON_ERR} "Error: Remove of mautic/core-project-message from Mautic${MAUTIC_COUNT} failed. We continue."
 else
-  show_info ${ICON_OK} ' done.' 0
+  show_info ${ICON_OK} 'done.' 0
 fi
 
 show_info ${ICON_INFO} 'Autoremove of not needed packages...'
 DEBIAN_FRONTEND=noninteractive apt-get -yq autoremove >/dev/null
-show_info ${ICON_OK} ' done.' 0
+show_info ${ICON_OK} 'done.' 0
 
 show_info ${ICON_INFO} 'Clear cache of installed packages...'
 DEBIAN_FRONTEND=noninteractive apt-get -yq clean >/dev/null
-show_info ${ICON_OK} ' done.' 0
+show_info ${ICON_OK} 'done.' 0
 
 show_info ${ICON_INFO} 'Clearing cache and setting permissions...'
 
-chown -R www-data:www-data "${MAUTIC_FOLDER}"
-chmod -R 755 "${MAUTIC_FOLDER}"
+chown -R www-data:www-data "${MAUTIC_FOLDER}" >/dev/null 2>&1
+chmod -R 755 "${MAUTIC_FOLDER}" >/dev/null 2>&1
 
-runuser -u www-data -- php "${CRON_FOLDER}cron-clear-cache.php"
+runuser -u www-data -- php "${CRON_FOLDER}cron-clear-cache.php" >/dev/null 2>&1
 
-show_info ${ICON_OK} ' done.'
+show_info ${ICON_OK} 'done.'
