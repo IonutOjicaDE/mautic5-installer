d49649cf392c7d6650486a06882ddb52298ce3a7
Ionut Ojica <contact@ionutojica.ro>
2025-04-26 19:59:34 +0200

    refactor: enhance error handling and feedback for ufw and fail2ban installation

--- scripts/script-e10-ufw-fail2ban.sh
+++ scripts/script-e10-ufw-fail2ban.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="0.0.2"
+VERSION="0.0.3"
 show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 ###############################################################################################
@@ -9,20 +9,44 @@ show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 if [ -z "${MAUTIC_COUNT}" ]; then
 
-  show_info ${ICON_INFO} 'Installing ufw (Firewall)...'
-  DEBIAN_FRONTEND=noninteractive apt-get -yq install ufw >/dev/null
-  ufw allow ssh 
-  ufw allow 80
-  ufw allow 443
-  ufw --force enable
-  show_info ${ICON_OK} 'ufw (Firewall) is installed.'
+  show_info ${ICON_INFO} 'Installing ufw (Firewall) and allow only ssh, http and https...'
+  errors=()
+  DEBIAN_FRONTEND=noninteractive apt-get -yq install ufw >/dev/null 2>&1 || errors+=("Installing ufw.")
+  ufw allow ssh >/dev/null 2>&1 || errors+=("ufw allow ssh.")
+  ufw allow 80 >/dev/null 2>&1 || errors+=("ufw allow 80.")
+  ufw allow 443 >/dev/null 2>&1 || errors+=("ufw allow 443.")
+  ufw --force enable >/dev/null 2>&1 || errors+=("ufw --force enable.")
+
+  if [[ ${#errors[@]} -gt 0 ]]; then
+    show_info ${ICON_ERR} "ERROR:"
+    for err in "${errors[@]}"; do
+      show_info ${ICON_NOGO} "$err"
+    done
+
+    show_info ${ICON_QUE} "Should we continue installation?"
+    answer_yes_else_stop
+  else
+    show_info ${ICON_OK} 'done.' 0
+  fi
 
   show_info ${ICON_INFO} 'Installing fail2ban (against BruteForce attacks)...'
-  DEBIAN_FRONTEND=noninteractive apt-get -yq install fail2ban >/dev/null
-  cp /etc/fail2ban/jail.{conf,local}
-  sed -i "s|bantime  = 10m|bantime  = 1d|g" /etc/fail2ban/jail.local
-  systemctl restart fail2ban
-  show_info ${ICON_OK} 'fail2ban is installed.'
+  errors=()
+  DEBIAN_FRONTEND=noninteractive apt-get -yq install fail2ban >/dev/null 2>&1 || errors+=("Installing fail2ban.")
+  cp /etc/fail2ban/jail.{conf,local} >/dev/null 2>&1 || errors+=("Copy conf file.")
+  sed -i "s|bantime  = 10m|bantime  = 1d|g" /etc/fail2ban/jail.local >/dev/null 2>&1 || errors+=("Increase ban time from 10m to 1d.")
+  systemctl restart fail2ban >/dev/null 2>&1 || errors+=("systemctl restart fail2ban.")
+
+  if [[ ${#errors[@]} -gt 0 ]]; then
+    show_info ${ICON_ERR} "ERROR:"
+    for err in "${errors[@]}"; do
+      show_info ${ICON_NOGO} "$err"
+    done
+
+    show_info ${ICON_QUE} "Should we continue installation?"
+    answer_yes_else_stop
+  else
+    show_info ${ICON_OK} 'done.' 0
+  fi
 
 else
 
