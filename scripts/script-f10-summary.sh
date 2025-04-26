e4129bb150030fa7bf6d3b203aed63f4dc57474c
Ionut Ojica <contact@ionutojica.ro>
2025-04-26 20:00:27 +0200

    refactor: clean up output formatting and enhance error messages in summary script

--- scripts/script-f10-summary.sh
+++ scripts/script-f10-summary.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-VERSION="0.0.2"
+VERSION="0.0.3"
 show_info ${ICON_INFO} "Start executing ${install_script_file} V${VERSION}." 1
 
 ###############################################################################################
 #####                                   Display summary                                   #####
 ###############################################################################################
 
-echo
-echo
-echo
-echo '==========================================================================================================================='
 echo
 echo "Mautic installation is finished!"
 if [ "${SSL_CERTIFICATE,,}" == "test" ]; then
@@ -23,22 +19,24 @@ else
   echo
 fi
 echo
-echo "You can login to Mautic here: https://${MAUTIC_SUBDOMAIN}"
-echo "use username: ${MAUTIC_USERNAME}"
-echo "and password: ${MAUTIC_ADMIN_PASSWORD}"
+echo -e "You can login to Mautic here: ${UBlu}https://${MAUTIC_SUBDOMAIN}${RCol}"
+echo -e "use username: ${BBlu}${MAUTIC_USERNAME}${RCol}"
+echo -e "and password: ${BBlu}${MAUTIC_ADMIN_PASSWORD}${RCol}"
 echo
 if [ "$EMAIL_SENT" = false ]; then
-  echo -e "${BRed}ERROR: The email with the passwords was not sent!"
-  echo "Please manually copy the passwords *now* from the file: ${CRON_FOLDER}mautic.txt !"
-  echo -e "The content is displayed also below:${RCol}"
+  echo -e "${BRed}ERROR: The email with the passwords was not sent!${RCol}"
+  echo "Please manually copy the passwords *NOW* from the file: ${CRON_FOLDER}mautic.txt !"
+  echo "The content is displayed also below:"
   cat "${CRON_FOLDER}mautic.txt"
 else
-  echo "Passwords created during installations were sent to $(check_positive "${SEND_PASS_TO_SENDER_EMAIL}" && echo "${SENDER_EMAIL} and to ")${ADMIN_EMAIL}"
+  echo -e "Passwords created during installations were sent to $(check_positive "${SEND_PASS_TO_SENDER_EMAIL}" && echo "${UBlu}${SENDER_EMAIL}${RCol} and to ")${UBlu}${ADMIN_EMAIL}${RCol}"
 fi
 echo
-echo '==========================================================================================================================='
-echo 'Updated for Mautic 5 by Ionuţ Ojică - contact@ionutojica.ro'
+echo -e "Root password to login trough ssh: ${BBlu}${ROOT_USER_PASSWORD}${RCol}"
+echo
+echo '===================================================================================================='
+echo 'Updated for Mautic 5 and 6 by Ionuţ Ojică - contact@ionutojica.ro'
 echo
 echo 'Source created by Matthias Reich - Info@Online-Business-Duplicator.de ( https://online-business-duplicator.de/mautic ).'
 echo
-echo '==========================================================================================================================='
+echo '===================================================================================================='
